15484219a676b00ae7e62e44e7031cfe1d1da9af
pgreene <prdgreene@gmail.com>
2014-02-14 10:15:43 -0500

    updated default blacklist parameter

--- definitions/sumo_source.rb
+++ definitions/sumo_source.rb
@@ -47,7 +47,7 @@ define :sumo_source, :path => nil do
       tzoverride = params[:force_timezone] ||
                    node[:sumologic][:sources][:force_timezone] || false
       blist params[:blacklist] ||
-            node[:sumologic][:sources][:default_blacklist] || '/path/to/nothing/nothing.log'
+            node[:sumologic][:sources][:default_blacklist] || ''
       node.run_state[:sumo_source][toadd] =
         { :path => params[:path],
           :category => cat,
